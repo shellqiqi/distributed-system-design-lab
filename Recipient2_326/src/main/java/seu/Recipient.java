8888d84878b01fbbca9e2cf5290213b5be3e2b97
shell7 <shellqiqi@outlook.com>
2018-03-21 22:13:10 +0800

    Complete Recipient 2

--- Recipient2_326/src/main/java/seu/Recipient.java
+++ Recipient2_326/src/main/java/seu/Recipient.java
@@ -1,7 +1,6 @@
 package seu;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.SimpleDateFormat;
@@ -10,6 +9,8 @@ import java.util.Date;
 public class Recipient {
 
     public void start(int port) throws IOException, ClassNotFoundException {
+        FileOutputStream fileOS = new FileOutputStream(new File("result.txt"));
+        BufferedOutputStream bufferedOS = new BufferedOutputStream(fileOS);
         ServerSocket serverSocket = new ServerSocket(port);
         Socket socket = serverSocket.accept();
         ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
@@ -19,20 +20,25 @@ public class Recipient {
             if (object != null) {
                 Message received = (Message)object;
                 if (received.getRandom1() == 2) break;
-                logLine(received);
+                bufferedOS.write(logLine(received).getBytes());
+                bufferedOS.flush();
             } else break;
         }
         inputStream.close();
         socket.close();
         serverSocket.close();
+        bufferedOS.close();
+        fileOS.close();
         System.out.println("Done.");
     }
 
-    private void logLine(Message message) {
-        System.out.printf("%-10d%-10d%-15s%-15s\n",
+    private String logLine(Message message) {
+        String s = String.format("%-10d%-10d%-15s%-15s\n",
                 message.getRandom1(),
                 message.getRandom2(),
                 message.getTimer(),
                 new SimpleDateFormat("HH:mm:ss:SSS").format(new Date()));
+        System.out.print(s);
+        return s;
     }
 }
